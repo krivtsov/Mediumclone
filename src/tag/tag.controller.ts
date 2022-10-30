a7f57177b4d25635242dca57c4a02bd8f770f418
Krivtsov <krivtsov.su@gmail.com>
2022-10-30 22:57:11 +0400

    add line

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -4,6 +4,7 @@ import { TagService } from './tag.service';
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
+
   @Get()
   findAll(): string[] {
     return this.tagService.findAll();
