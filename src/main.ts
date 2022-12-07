fbfc40188c2f8a26f4d62b2923fafc7dca4b3200
Krivtsov <krivtsov.su@gmail.com>
2022-12-07 23:30:19 +0400

    mode loging

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'verbose'],
+  });
   await app.listen(3039);
 }
 
