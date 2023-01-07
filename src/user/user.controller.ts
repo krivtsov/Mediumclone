11120e36699e37f4e6e80fe8154164391b9bef2c
Krivtsov <krivtsov.su@gmail.com>
2023-01-08 00:42:00 +0400

    add user entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { UserService } from '@app/user/user.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserEntity } from '@app/user/user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserEntity> {
     return this.userService.createUser(createUserDto);
   }
 }
